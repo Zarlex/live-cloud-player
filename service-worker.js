ab683cdc2c467ecac83f02a768401f939ef79339
zarlex <alex.zarges@online.de>
2018-04-22 04:28:08 +0200

    Release new version

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/index.html","1253634d063c6a43065478011736a6be"],["/inline.318b50c57b4eba3d437b.bundle.js","6eaa1608803b51f7d836604d9585670d"],["/main.697c80779a278b47506c.bundle.js","cd60a1f3a3b5b117480ec86daa8a3682"],["/polyfills.21b4a7e14a785973cc75.bundle.js","e129536cf17017282c15a2294a8447ba"],["/styles.e8cba79683ba82784208.bundle.css","8e076dba8843b48ffad866b5b75f3cbc"]];
+var precacheConfig = [["/index.html","8adcbb8f1516ec825032f6a20601bf72"],["/inline.318b50c57b4eba3d437b.bundle.js","6eaa1608803b51f7d836604d9585670d"],["/main.8b28755e970e9074ac86.bundle.js","a8b48300fe37dbd229a33d539268280b"],["/polyfills.21b4a7e14a785973cc75.bundle.js","e129536cf17017282c15a2294a8447ba"],["/styles.e8cba79683ba82784208.bundle.css","8e076dba8843b48ffad866b5b75f3cbc"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
