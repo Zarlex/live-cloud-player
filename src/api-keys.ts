5240549369bde1ad1e5298ed626c7d71cd04dc9f
zarlex <alex.zarges@online.de>
2018-04-25 16:18:12 +0200

    ignore api-keys file

--- src/api-keys.ts
+++
@@ -1,5 +0,0 @@
-export const ApiKeys = {
-  youtubeClientId: 'YOUTUBE-API-KEY', //https://developers.google.com/youtube/v3/docs/ Get the API key here: https://console.developers.google.com/
-  microsoftKey: 'BING-API-KEY', //https://www.bingmapsportal.com/
-  timezoneDbKey: 'CODE'// https://timezonedb.com/register
-};
